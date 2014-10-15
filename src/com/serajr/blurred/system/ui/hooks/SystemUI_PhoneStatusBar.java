9144495ed72a95cefc2adaaff46f0a79fdf0e522
Serafin A. Albiero Jr. <serajr@gmail.com>
2014-10-15 08:54:11 -0300

    v1.9

--- src/com/serajr/blurred/system/ui/hooks/SystemUI_PhoneStatusBar.java
+++ src/com/serajr/blurred/system/ui/hooks/SystemUI_PhoneStatusBar.java
@@ -63,91 +63,91 @@ public class SystemUI_PhoneStatusBar {
 					// receiver
 					BroadcastReceiver receiver = new BroadcastReceiver() {
             			
-                        @Override
-                        public void onReceive(Context context, Intent intent) {
-                        	
-                        	String action = intent.getAction();
-                        	Handler handler = new Handler();
-                        	
-    						// alterou a rotação
-                        	if (action.equals(Intent.ACTION_CONFIGURATION_CHANGED)) {
-                        		
-                        		// recents
-                        		SystemUI_RecentsPanelView.onConfigurationChanged();
-                        		
-                        		// -----------------------------------------------------------------------
-        						// se na rotação do celular o mod estiver habilitado e o painel expandido
-        						// estiver aberto, fecha o painel expandido, forçando o usuário a expandir
-        						// o painel novamente para obtér a imagem desfocada com a rotação atual !!
-        						// -----------------------------------------------------------------------
-                        		
-        						// obtém os campos
-        						boolean mExpandedVisible = XposedHelpers.getBooleanField(param.thisObject, "mExpandedVisible");
-        						
-        						// habilitado ?
-        						if (mExpandedVisible &&
-        							SystemUI_NotificationPanelView.mBlurredStatusBarExpandedEnabled) {
-        					
-        							// fecha o painel
-        							XposedHelpers.callMethod(param.thisObject, 
-        									Utils.getAndroidAPILevel() >= 17
-        										// >= 4.2
-        										? "makeExpandedInvisible"
-        										// <= 4.1
-        										: "performCollapse");
-        						
-        						}
-                        	}
-                        	
-                        	// atualiza
-                        	if (action.equals(BlurSettings_Fragment.BLURRED_SYSTEM_UI_UPDATE_INTENT)) {
-                        		
-                            	handler.postDelayed(new Runnable() {
-                                	
-                                    @Override
-                                    public void run() {
-                                    	
-                                    	// recarregam as preferências
-                                		Xposed.getXposedXSharedPreferences().reload();
-                                		
-                                		// atualizam as preferências
-                                		updatePreferences();
-                    					
-                                    }
-                                }, 100);
-                        	}
-                        	
-                        	// mata o SystemUI.apk
-                        	if (action.equals(BlurSettings_Activity.BLURRED_SYSTEM_UI_KILL_SYSTEM_UI_INTENT)) {
-				
-                        		// atrasa em meio segundo
-                        		handler.postDelayed(new Runnable() {
-                                	
-                                    @Override
-                                    public void run() {
-                                    	
-                                    	// mata
-                                    	Process.sendSignal(Process.myPid(), Process.SIGNAL_KILL);
-                                    	
-                                    }
-                             	}, 100);
-                        	}
-                        }
-                    };
+			                        @Override
+			                        public void onReceive(Context context, Intent intent) {
+			                        	
+			                        	String action = intent.getAction();
+			                        	Handler handler = new Handler();
+			                        	
+			    				// alterou a rotaÃ§Ã£o
+			                        	if (action.equals(Intent.ACTION_CONFIGURATION_CHANGED)) {
+			                        		
+			                        		// recents
+			                        		SystemUI_RecentsPanelView.onConfigurationChanged();
+			                        		
+			                        		// -----------------------------------------------------------------------
+	        						// se na rotaÃ§Ã£o do celular o mod estiver habilitado e o painel expandido
+	        						// estiver aberto, fecha o painel expandido, forÃ§ando o usuÃ¡rio a expandir
+	        						// o painel novamente para obtÃ©r a imagem desfocada com a rotaÃ§Ã£o atual !!
+	        						// -----------------------------------------------------------------------
+	                        		
+	        						// obtÃ©m os campos
+	        						boolean mExpandedVisible = XposedHelpers.getBooleanField(param.thisObject, "mExpandedVisible");
+	        						
+	        						// habilitado ?
+	        						if (mExpandedVisible &&
+	        							SystemUI_NotificationPanelView.mBlurredStatusBarExpandedEnabled) {
+	        					
+	        							// fecha o painel
+	        							XposedHelpers.callMethod(param.thisObject, 
+	        								Utils.getAndroidAPILevel() >= 17
+	        									// >= 4.2
+	        									? "makeExpandedInvisible"
+	        									// <= 4.1
+	        									: "performCollapse");
+	        						
+	        						}
+			                        	}
+			                        	
+			                        	// atualiza
+			                        	if (action.equals(BlurSettings_Fragment.BLURRED_SYSTEM_UI_UPDATE_INTENT)) {
+			                        		
+				                            	handler.postDelayed(new Runnable() {
+				                                	
+				                                    	@Override
+				                                    	public void run() {
+				                                    	
+				                                    		// recarregam as preferÃªncias
+				                                		Xposed.getXposedXSharedPreferences().reload();
+				                                		
+				                                		// atualizam as preferÃªncias
+				                                		updatePreferences();
+				                    					
+				                                    	}
+				                                }, 100);
+			                        	}
+			                        	
+			                        	// mata o SystemUI.apk
+			                        	if (action.equals(BlurSettings_Activity.BLURRED_SYSTEM_UI_KILL_SYSTEM_UI_INTENT)) {
+							
+			                        		// atrasa em meio segundo
+			                        		handler.postDelayed(new Runnable() {
+			                                	
+				                                    	@Override
+				                                    	public void run() {
+				                                    	
+				                                    		// mata
+				                                    		Process.sendSignal(Process.myPid(), Process.SIGNAL_KILL);
+				                                    	
+				                                    	}
+				                             	}, 100);
+			                        	}
+			                    	}
+					};
                     	
-                    // registra o receiver
-                    IntentFilter intent = new IntentFilter();
-                    intent.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
-                    intent.addAction(BlurSettings_Fragment.BLURRED_SYSTEM_UI_UPDATE_INTENT);
-                    intent.addAction(BlurSettings_Activity.BLURRED_SYSTEM_UI_KILL_SYSTEM_UI_INTENT);
-                    mStatusBarWindow.getContext().registerReceiver(receiver, intent);
+			                // registra o receiver
+			                IntentFilter intent = new IntentFilter();
+			                intent.addAction(Intent.ACTION_CONFIGURATION_CHANGED);
+			                intent.addAction(BlurSettings_Fragment.BLURRED_SYSTEM_UI_UPDATE_INTENT);
+			                intent.addAction(BlurSettings_Activity.BLURRED_SYSTEM_UI_KILL_SYSTEM_UI_INTENT);
+			                mStatusBarWindow.getContext().registerReceiver(receiver, intent);
                     
-            		// atualizam as preferências
-            		updatePreferences();
+		            		// atualizam as preferÃªncias
+		            		updatePreferences();
             		
-            		// fundos transparentes (preferências necessitam de reboot)
-            		// então somente passa por aqui no primeiro boot da rom !!!
-            		SystemUI_TranslucentBackground.handleTranslucentBackgroundPrefs();
+		            		// fundos transparentes (preferÃªncias necessitam de reboot)
+		            		// entÃ£o somente passa por aqui no primeiro boot da rom !!!
+		            		SystemUI_TranslucentBackground.handleTranslucentBackgroundPrefs();
             		
 				}
 			});
@@ -168,7 +168,7 @@ public class SystemUI_PhoneStatusBar {
 					if (!SystemUI_NotificationPanelView.mBlurredStatusBarExpandedEnabled)
 						return;
 					
-					// não continua se o blur ja foi aplicado (previne um segundo blur) !!!
+					// nÃ£o continua se o blur ja foi aplicado (previne um segundo blur) !!!
 					if (SystemUI_NotificationPanelView.mBlurredView.getTag().toString().equals("blur_applied"))
 						return;
 					
@@ -183,7 +183,7 @@ public class SystemUI_PhoneStatusBar {
 					// <= 4.1
 					if (Utils.getAndroidAPILevel() <= 16) {
 						
-						// obtém os campos
+						// obtÃ©m os campos
 						View mCloseView = (View) XposedHelpers.getObjectField(param.thisObject, "mCloseView");
 						
 						// seta o alpha
